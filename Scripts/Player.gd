7a52dd378ee72bc8da7a3ae586708d141a475ffd
mwlegath <mlegath92@gmail.com>
2021-10-11 21:29:29 -0400

    project initiation
    added player, player script, level 1, tile map, and background

---
+++ Scripts/Player.gd
@@ -0,0 +1,39 @@
+extends KinematicBody2D
+
+# Physics 
+var speed : int = 200 
+var jumpForce : int = 450
+var gravity : int = 800
+var vel : Vector2 = Vector2()
+var grounded : bool = false
+
+# components 
+onready var sprite = $AnimatedSprite
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+func _physics_process(delta):
+	
+	# reset horizontal velocity 
+	vel.x = 0
+	
+	# movement 
+	if Input.is_action_pressed("move_left"):
+		vel.x -= speed
+	if Input.is_action_pressed("move_right"):
+		vel.x += speed 
+	if Input.is_action_pressed("jump") and is_on_floor():
+		vel.y -= jumpForce
+	
+	# applying velocity 
+	vel = move_and_slide(vel, Vector2.UP)
+	
+	#gravity 
+	vel.y += gravity * delta
+	
+	# sprite direction 
+	
+		
